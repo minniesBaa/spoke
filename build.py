26dcbb921a75920b90dac5afa769a16767095085
minniesBaa <julianjohnsontaylor@gmail.com>
2025-08-26 11:57:15 -0700

    add howtoplay to build

--- build.py
+++ build.py
@@ -2,3 +2,4 @@ import shutil
 shutil.copy("puzzle.txt", "public/puzzle.json")
 shutil.copy("index.html", "public/index.html")
 shutil.copy("wordgen_assets/plural.json", "public/words.json")
+shutil.copy("howtoplay.html", "public/howtoplay.html")

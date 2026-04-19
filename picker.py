622e06815e277262724c82e4b33520fa27c3ccf6
minniesBaa <julianjohnsontaylor@gmail.com>
2026-04-19 11:08:42 -0700

    prevent infinite loops if there are not enough words with th e same middle letter

--- picker.py
+++ picker.py
@@ -7,10 +7,14 @@ data = words.splitlines()
 getword = lambda: random.choice(data)
 wl = [getword()]
 middlechar = wl[0][2]
+itr = 0
 for i in range(3):
     word = "     "
     while (word[2] != middlechar) or ("q" if i == 0 else "valuethatwonteverbeinaword") in word:
         word = getword()
+        itr += 1
+        if itr > 20000:
+            exit()
     wl.append(word)
 
 out = json.dumps(wl)
